f41c26f58c62e9495a4eb1fbfa5b08f97c93a339
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-16 14:24:50 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
-function berechne_rad_werte () {
+function berechne_rad_werte() {
     kurve_links = kurve_rad * -1
     kurve_rechts = kurve_rad
-    gerade_links = gerade_rad
-    gerade_rechts = gerade_rad * rad_rechts_korrektur
+    gerade_links = gerade_rad * rad_links_korrektur
+    gerade_rechts = gerade_rad
     links_soll = Math.round((gerade_links + kurve_links * feinheit) / 2)
     rechts_soll = Math.round((gerade_rechts + kurve_rechts * feinheit) / 2)
 }
-// robotbit.Servo(robotbit.Servos.S1, 0)
-function init () {
+
+function init() {
     radio.setGroup(26)
     basic.showIcon(IconNames.Diamond)
     hebe_winkel = 70
     motor_rechts = robotbit.Motors.M1A
-motor_links = robotbit.Motors.M2A
-rad_rechts_korrektur = 1
-    feinheit = 0.7
+    motor_links = robotbit.Motors.M2A
+    rad_links_korrektur = 1
+    // normal: 0.5
+    feinheit = 0.5
     robotbit.MotorStopAll()
+    // robotbit.Servo(robotbit.Servos.S1, 0)
 }
 radio.onReceivedValue(function (info, wert) {
     // serial.writeValue("kurve_rad", kurve_rad)
@@ -37,9 +39,9 @@ input.onButtonPressed(Button.B, function () {
     // robotbit.Servo(robotbit.Servos.S1, 0)
     // robotbit.Servo(robotbit.Servos.S1, hebe_winkel)
     if (oben) {
-    	
+
     } else {
-    	
+
     }
     oben = !(oben)
 })
@@ -50,21 +52,20 @@ let hebe_winkel = 0
 let rechts_soll = 0
 let links_soll = 0
 let gerade_rechts = 0
+let rad_links_korrektur = 0
 let gerade_rad = 0
 let gerade_links = 0
 let kurve_rechts = 0
 let kurve_rad = 0
 let kurve_links = 0
-let rad_rechts_korrektur = 0
 let feinheit = 0
-let rechts_ist = 0
-let links_ist = 0
-let motor_rechts = 0
-let motor_links = 0
-let rad_rechts = 0
 let rad_links = 0
+let rad_rechts = 0
+let motor_links = 0
+let motor_rechts = 0
+let links_ist = 0
+let rechts_ist = 0
 feinheit = 0.5
-rad_rechts_korrektur = 1
 init()
 basic.forever(function () {
     berechne_rad_werte()
@@ -82,7 +83,7 @@ basic.forever(function () {
         robotbit.MotorStopAll()
     } else {
         robotbit.MotorRun(motor_links, links_ist)
-robotbit.MotorRun(motor_rechts, rechts_ist)
+        robotbit.MotorRun(motor_rechts, rechts_ist)
     }
     basic.pause(10)
 })
